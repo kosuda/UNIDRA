690fa220c687897c4bd0cf1c7a00040e32217453
kosuda_tatsuya <kosuda_tatsuya@cyberagent.co.jp>
2015-03-24 15:08:43 +0900

    Format code

--- Assets/Scripts/CharaAnimation.cs
+++ Assets/Scripts/CharaAnimation.cs
@@ -2,57 +2,55 @@
 
 public class CharaAnimation : MonoBehaviour
 {
-	Animator animator;
-	CharacterStatus status;
-	Vector3 prePosition;
-	bool isDown = false;
-	bool attacked = false;
-	
-	public bool IsAttacked()
-	{
-		return attacked;
-	}
-	
-	void StartAttackHit()
-	{
-		Debug.Log ("StartAttackHit");
-	}
-	
-	void EndAttackHit()
-	{
-		Debug.Log ("EndAttackHit");
-	}
-	
-	void EndAttack()
-	{
-		attacked = true;
-	}
-	
-	void Start ()
-	{
-		animator = GetComponent<Animator>();
-		status = GetComponent<CharacterStatus>();
+  Animator animator;
+  CharacterStatus status;
+  Vector3 prePosition;
+  bool isDown = false;
+  bool attacked = false;
+
+  public bool IsAttacked ()
+  {
+    return attacked;
+  }
+
+  void StartAttackHit ()
+  {
+    Debug.Log ("StartAttackHit");
+  }
+
+  void EndAttackHit ()
+  {
+    Debug.Log ("EndAttackHit");
+  }
+
+  void EndAttack ()
+  {
+    attacked = true;
+  }
+
+  void Start ()
+  {
+    animator = GetComponent<Animator> ();
+    status = GetComponent<CharacterStatus> ();
 		
-		prePosition = transform.position;
-	}
-	
-	void Update ()
-	{
-		Vector3 delta_position = transform.position - prePosition;
-		animator.SetFloat("Speed", delta_position.magnitude / Time.deltaTime);
+    prePosition = transform.position;
+  }
+
+  void Update ()
+  {
+    Vector3 delta_position = transform.position - prePosition;
+    animator.SetFloat ("Speed", delta_position.magnitude / Time.deltaTime);
 		
-		if(attacked && !status.attacking)
-		{
-			attacked = false;
-		}
-		animator.SetBool("Attacking", (!attacked && status.attacking));
+    if (attacked && !status.attacking) {
+      attacked = false;
+    }
+    animator.SetBool ("Attacking", (!attacked && status.attacking));
 		
-		if(!isDown && status.died)
-		{
-			isDown = true;
-			animator.SetTrigger("Down");
-		}
+    if (!isDown && status.died) {
+      isDown = true;
+      animator.SetTrigger ("Down");
+    }
 		
-		prePosition = transform.position;
-	}
+    prePosition = transform.position;
+  }
 }
