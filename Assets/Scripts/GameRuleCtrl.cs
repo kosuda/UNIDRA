3abf03844b0c616c02cfd9a7607c0d57522090a3
kosuda_tatsuya <kosuda_tatsuya@cyberagent.co.jp>
2015-03-25 19:53:47 +0900

    Chapter14

--- Assets/Scripts/GameRuleCtrl.cs
+++ Assets/Scripts/GameRuleCtrl.cs
@@ -1,33 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameRuleCtrl : MonoBehaviour
-{
-  // 残り時間
-  public float timeRemaining = 5.0f * 60.0f;
-  // ゲームオーバーフラグ
-  public bool gameOver = false;
-  // ゲームクリア
-  public bool gameClear = false;
+public class GameRuleCtrl : MonoBehaviour {
+	// 残り時間
+	public float timeRemaining = 5.0f * 60.0f;
+	// ゲームオーバーフラグ
+	public bool gameOver = false;
+	// ゲームクリア
+    public bool gameClear = false;
+	// シーン移行時間
+	public float sceneChangeTime = 3.0f;
 
-  void Update ()
-  {
-    timeRemaining -= Time.deltaTime;
-    // 残り時間が無くなったらゲームオーバー
-    if (timeRemaining <= 0.0f) {
-      GameOver ();
-    }
-  }
+	void Update()
+	{
+		// ゲーム終了条件成立後、シーン遷移
+		if( gameOver || gameClear ){
+			sceneChangeTime -= Time.deltaTime;
+			if( sceneChangeTime <= 0.0f ){
+				Application.LoadLevel("TitleScene");
+			}
+			return;
+		}
 
-  public void GameOver ()
-  {
-    gameOver = true;
-    Debug.Log ("GameOver");
-  }
+		timeRemaining -= Time.deltaTime;
+		// 残り時間が無くなったらゲームオーバー
+		if(timeRemaining<= 0.0f ){
+			GameOver();
+		}
 
-  public void GameClear ()
-  {
-    gameClear = true;
-    Debug.Log ("GameClear");
-  }
+	}
+	
+	public void GameOver()
+	{
+		gameOver = true;
+	}
+	public void GameClear()
+	{
+		gameClear = true;
+	}
 }
